f76fb35dba5f55ba3226341eec041fe3b9938ccf
SamGit08 <samboudiallo@ymail.com>
2017-06-08 17:54:37 +0200

    ajout confirmation

--- pages/long_trajet.php
+++ pages/long_trajet.php
@@ -30,27 +30,7 @@
         <button id="annulationLT" class="boutonModal" type="button" name="button">Annuler</button>
       </div>
     </form>
-<<<<<<< HEAD
   </div>
-=======
-    <div class="confirmationLT">
-      <div class="modalLT">
-         <h1 class="titreReservation">votre reservation</h1>
-          <div class="infosResa">
-     <p>lieu de prise en charge: <?php echo $_POST['origineLt']; ?></p>
-     <p> date : <?php echo $_POST['dateLt']; ?></p>
-     <p>heure: <?php echo $_POST['horairesLt']; ?></p>
-      <p>etapes de votre trajet : <?php echo $_POST['choixLt']; ?></p>
-      <p>votre destination: <?php echo $_POST['destinationLt']; ?></p>
-          </div>
-          <div class="boutons">
-            <button id="annulationLT" class="boutonModal" type="button" name="button">Annuler</button>
-            <button class="boutonModal" type="button" name="button">Confirmer</button>
-          </div>
-      </div>
-    </div>
-</div>
->>>>>>> 960f8663f9b004c7613b66bb7e664dc2adc265c4
 <?php require 'mode_paiement.php'; ?>
 <script type="text/javascript" src="../public/js/animation.js"></script>
 <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
